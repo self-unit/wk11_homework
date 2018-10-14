4f2cf019391fa176178df4125ab2ef584e753d03
self-unit <self_unit@hotmail.com>
2018-10-14 17:59:33 +0100

    first commit, plane hasPassenger test passing

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,10 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Plane {
     private PlaneType type;
     private Airline airline;
+    private ArrayList<Passenger> passengers;
 
     public Plane(PlaneType type, Airline airline) {
         this.type = type;
         this.airline = airline;
+        this.passengers = new ArrayList<>();
     }
 
     public PlaneType getType() {
@@ -13,5 +18,13 @@ public class Plane {
 
     public Airline getAirline() {
         return airline;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
     }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PlaneTest {
+    Passenger passenger;
     Plane plane;
 
     @Before
     public void before() {
+        passenger = new Passenger("Steve");
         plane = new Plane(PlaneType.BOEING737, Airline.CATHAY_PACIFIC);
     }
 
@@ -20,4 +22,11 @@ public class PlaneTest {
     public void hasAirline() {
         assertEquals(Airline.CATHAY_PACIFIC, plane.getAirline());
     }
+
+    @Test
+    public void canAddPassenger() {
+        plane.addPassenger(passenger);
+        assertEquals(1, plane.getPassengers().size());
+    }
+
 }
